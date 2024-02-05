4de06283520154dabe4d74c00704aa780a77bf6e
Luciano Ayres <lucianofarias@gmail.com>
2024-02-04 21:11:45 -0300

    chore: update dummy data URL

--- templates/fetch-example/popup/popup.js
+++ templates/fetch-example/popup/popup.js
@@ -1,6 +1,7 @@
-console.log("This is a popup")
+console.log('This is a popup')
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+dummyDataURL = 'https://raw.githubusercontent.com/lucianoayres/browser-extensions-templates/dbfaa41c3d5e143076cf3fed034f4e91a40ca506/templates/fetch-example/dummy-data/data.json'
 
+fetch(dummyDataURL)
+    .then(response => response.json())
+    .then(json => console.log(json))
